4d5feb3115579f439e881c02fdcac78effe06518
larafdezmorena <larafdezmorena@gmail.com>
2024-03-09 14:06:59 +0100

    create detail file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@
 
 const express = require("express");
 const cors = require("cors");
-//const path = require ('path');
+const path = require ('path');
 
 const mysql = require("mysql2/promise");
-const { log } = require("console");
 require("dotenv").config();
 
 //CREAR VARIABLES
@@ -15,17 +14,17 @@ require("dotenv").config();
 const server = express();
 const port = 3000; // 1025 - 65365
 
-// http://   localhost  :  3000   /   path
 
 //CONFIGURACIÓN
 
 server.use(cors());
-server.use(express.json());
+server.use(express.json({limit: '25mb'}));
+server.set('view engine', 'ejs');
 
 //CONFIGURACIÓN DE MYSQL
 
-// .env variables de entorno
 async function getConnection() {
+
   const connection = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DB,
@@ -51,7 +50,7 @@ server.listen(port, () => {
 // ENDPOINTS
 
 //Crear projectos
-server.post("/api/projectCard", async (req, rest) => {
+server.post('/api/projectCard', async (req, res) => {
   // Datos vienen req.body
 
   //1.conectar a lqa base de datos
@@ -64,19 +63,22 @@ server.post("/api/projectCard", async (req, rest) => {
 INSERT author (name, job, image)
 VALUES (?, ?, ?)`;
 
-  conn.execute(insertAuthor, [req.body.name, req.body.job, req.body.image]);
+const [resultsInsertAuthor] = await conn.execute(
+  insertAuthor, 
+  [req.body.name, req.body.job, req.body.image]);
 
   //3.recupero el id de Authors
 
   console.log(resultsInsertAuthor.insertId);
+  const fkAuthor = resultsInsertAuthor.insertId;
 
   //4.insertar el proyecto. añadir fkauthor en project. el campo idauthor de la tabla author esta realcionado. dos inserts
 
-  const insertProject = `
+const insertProject = `
 INSERT projects (title, slogan, repo, demo, technologies, \`description\`, image, fkAuthor)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-  const [resuktsInsertProject] = await conn.execute(insertProject, [
+  const [resultsInsertProject] = await conn.execute(insertProject, [
     req.body.title,
     req.body.slogan,
     req.body.repo,
@@ -89,10 +91,14 @@ INSERT projects (title, slogan, repo, demo, technologies, \`description\`, image
 
   //5.Recupero el id de project
 
-  const idProject = resultsInsertProject.insertId;
+  const idProject = resultsInsertProject.insertId
+
   //6.cierro la conexión
+
   conn.end();
+
   //7.devuelvo el json
+
   res.json({
     sucess: true,
     cardURL: `http://localhost:${port}/projectCard/${idProject}`,
@@ -107,6 +113,7 @@ server.get("/api/projectCard", (req, res) => {
   //4. Devuelvo un json con los resultados
 });
 
+
 //Mostrar detalle projecto (servidor de dinámicos)
 server.get("/projectCard/:id", async (req, res) => {
   // Recibo el id del proyecto en un URL param
@@ -114,45 +121,30 @@ server.get("/projectCard/:id", async (req, res) => {
   //1. Conectar con la BD
 
   const conn = await getConnection();
+
   //2. Lanzar SELECT para recuperar 1 proyecto con el id <- req.params
 
-  const selectProjects = `
+const selectProjects = `
 SELECT *
     FROM author a
         JOIN projects p ON (a.idAuthor = p.fkAuthor)
     WHERE p.idProject = ?
 ;
 `;
-  const [results] = await conn.query(selectProjects, [req.params.id]); //repsar el req.params
+  const [results] = await conn.query(selectProjects, [req.params.id]);
 
   //3. Hago un template (EJS)
+
   //4. Cierro conexion
+
   conn.end();
+
   //5. res.render ('plantiilla', resultado)
 
-  const data = {};
+  const data = results [0];
 
   res.render(`detail`, data);
-});
 
-//   GET   http://localhost:3000/project
-
-server.get(`/project`, async (req, res) => {
-  console.log("pidiendo a la base de datos información de las tarjetas");
-
-  //LANZAR UN SELECT
-
-  let sql = "SELECT * FROM project;";
-
-  // CONECTAR CON LA BASE DE DATOS
-
-  const connection = await getConnection();
-  const [results] = await connection.query(sql);
-  res.json(results);
-
-  // CERRAMOS LA CONEXIÓN
-
-  connection.end();
 });
 
 /*async function api() {
@@ -162,4 +154,5 @@ server.get(`/project`, async (req, res) => {
 
 // SERVIDOR ESTÁTICOS
 
-server.use(express.static("./public"));
+const staticServerPathWeb = '../public'; // En esta carpeta (en la raíz del proy) ponemos los ficheros estáticos
+server.use(express.static(path.join(__dirname, staticServerPathWeb)));
