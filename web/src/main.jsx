37bc470feb0f8412f1eb4ab66b005b3a76fa4d63
Esther García <garbennes@gmail.com>
2024-03-19 19:57:20 +0100

    Correct render url

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,17 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-// Obtiene el hostname de la URL actual
-const hostname = window.location.hostname;
-
-// Define el basename basado en el hostname
-let basename = "/";
-if (hostname === "localhost") {
-  basename = "/";
-} else {
-  // Si el hostname no es localhost, se asume que es el dominio de producción
-  basename = "/project-promo-apt-module-4-team-4.onrender.com"; // Cambia a la ruta correspondiente en tu dominio de producción
-}
+const basename = "/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
